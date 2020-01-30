07d4e80b355b4731247f24e8314affa0e7aaf54b
Sven Truschel <truschelsven@gmail.com>
2020-01-30 15:34:54 +0100

    'doehetlz'

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const http = require('http');
 const app = require('./app');
 
 const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
 
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 
 const server = http.createServer(app);
 server.listen(port);
-console.log('Server is running on ' + port)
+console.log('Server is running on ' + port);
