255330bfc677f2ec9df38fe6460bae83940f77c5
Sven Truschel <truschelsven@gmail.com>
2020-02-01 13:58:39 +0100

    enabled https

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const http = require('http');
+const http = require('https');
 const app = require('./app');
+const fs = require('fs')
 
 const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
+var key = fs.readFileSync(__dirname + '/../certs/selfsigned.key');
+var cert = fs.readFileSync(__dirname + '/../certs/selfsigned.crt');
+var options = {
+    key: key,
+    cert: cert
+};
+
 app.get('*', (request, response) => {
     response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 
-const server = http.createServer(app);
+const server = https.createServer(options, app);
 server.listen(port);
 console.log('Server is running on ' + port);
